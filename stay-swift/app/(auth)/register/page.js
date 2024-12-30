53bd8b8d97e984aea9b23ff0238fb544a06bfecc
Rahat <rakibulalamrahat24@gmail.com>
2024-12-30 09:53:17 +0600

    fixed register

--- stay-swift/app/(auth)/register/page.js
+++ stay-swift/app/(auth)/register/page.js
@@ -5,9 +5,9 @@ const RegistrationPage = () => {
   return (
     <section className="h-screen grid place-items-center">
       <div className="max-w-[450px] w-full mx-auto p-6 border border-gray-700/20 rounded-md">
-        <h4 className="font-bold text-2xl">Sign up</h4>
+        <h4 className="font-bold text-2xl mb-3">Sign up</h4>
         <RegistrationForm />
-        <SocialLogins mode={"register"}/>
+        <SocialLogins mode={"register"} />
       </div>
     </section>
   );
--- stay-swift/components/auth/RegistrationForm.jsx
+++ stay-swift/components/auth/RegistrationForm.jsx
@@ -1,73 +1,83 @@
-'use client'
+"use client";
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const RegistrationForm = () => {
   const [error, setError] = useState("");
-
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
-  async function onSubmit (event) {
+  async function onSubmit(event) {
+    event.preventDefault();
 
-      event.preventDefault();
-      try {
-        const formData = new FormData(event.currentTarget);
-        const fname = formData.get('fname');
-        const lname = formData.get('lname');
-        const email = formData.get('email');
-        const password = formData.get('password');
+    setIsLoading(true);
+    setError("");
 
-        const res = await fetch("/api/auth/register", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            fname,
-            lname,
-            email,
-            password,
-          }),
-        });
-        res.status === 201 &&
-          router.push("/login");
-      } catch (error) {
-        setError(error.message);
-      }
+    try {
+      const formData = new FormData(event.currentTarget);
+      const fname = formData.get("fname");
+      const lname = formData.get("lname");
+      const email = formData.get("email");
+      const password = formData.get("password");
+
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          fname,
+          lname,
+          email,
+          password,
+        }),
+      });
+      res.status === 201 && router.push("/login");
+    } catch (err) {
+      setError("An unexpected error occurred. Please try again.");
+    } finally {
+      setIsLoading(false);
     }
-
+  }
 
   return (
     <>
-      <div className="text-xl text-red-500 text-center">{error && error}</div>
+      {error && (
+        <div className="p-3 mb-4 text-sm text-red-500 bg-red-100 rounded">
+          {error}
+        </div>
+      )}
       <form className="login-form" onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="fname">First Name</label>
-        <input type="text" name="fname" id="fname" />
-      </div>
+        <div>
+          <label htmlFor="fname">First Name</label>
+          <input type="text" name="fname" id="fname" />
+        </div>
 
-      <div>
-        <label htmlFor="lname">Last Name</label>
-        <input type="text" name="lname" id="lname" />
-      </div>
+        <div>
+          <label htmlFor="lname">Last Name</label>
+          <input type="text" name="lname" id="lname" />
+        </div>
 
-      <div>
-        <label htmlFor="email">Email Address</label>
-        <input type="email" name="email" id="email" />
-      </div>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="email" id="email" />
+        </div>
 
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-      </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
 
-      <button type="submit" className="btn-primary w-full mt-4">
-        Create account
-      </button>
-    </form>
+        <button
+          disabled={isLoading}
+          type="submit"
+          className="btn-primary w-full mt-4"
+        >
+          Create account
+        </button>
+      </form>
     </>
-
   );
 };
 
